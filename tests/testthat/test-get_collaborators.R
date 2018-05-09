344763007c9fca97f5d8154b221b56331ddfb4c7
Maëlle Salmon <maelle@itsalocke.com>
2018-05-09 14:27:24 +0200

    skip this on Travis

--- tests/testthat/test-get_collaborators.R
+++ tests/testthat/test-get_collaborators.R
@@ -1,5 +1,6 @@
 context("test-get_collaborators.R")
 
 test_that("it returns a df", {
+  skip_on_travis()
   expect_is(get_collaborators("ropensci", "ropenaq"), "data.frame")
 })
--- tests/testthat/test-get_teams.R
+++ tests/testthat/test-get_teams.R
@@ -1,5 +1,6 @@
 context("test-get_teams.R")
 
 test_that("it returns a df", {
+  skip_on_travis()
   expect_is(get_teams("ropensci"), "data.frame")
 })
