3ffc914b26c4d7be8624dc0e5d80ba2baee97f9f
Irene <25118334+isteves@users.noreply.github.com>
2018-10-01 10:56:41 +0300

    added get_token use GITHUB_TOKEN or GITHUB_PAT if GITHUB_GRAPHQL_TOKEN is not found

--- R/zzz.R
+++ R/zzz.R
@@ -17,24 +17,32 @@ iterate <- function(query) {
   return(out)
 }
 
+#check for token; some calls work only with specific scopes
+get_token <- function() {
+  token <- Sys.getenv("GITHUB_GRAPHQL_TOKEN")
+  if(token == ""){
+    token <- Sys.getenv("GITHUB_TOKEN", Sys.getenv("GITHUB_PAT"))
+  }
+  if(token == ""){
+    stop("Please set your GITHUB_GRAPHQL_TOKEN environment variable.")
+  }
+  return(token)
+}
+
 # function for creating the client
 # it first checks whether there's a token
 # and then only creates the client if it doesn't already exist
 create_client <- function(){
-  token <- Sys.getenv("GITHUB_GRAPHQL_TOKEN")
-  if(token == ""){
-    stop("Please set your GITHUB_GRAPHQL_TOKEN environment variable.")
-  }else{
-    if(!exists("ghql_gh_cli")){
-      ghql_gh_cli <- ghql::GraphqlClient$new(
-        url = "https://api.github.com/graphql",
-        headers = httr::add_headers(Authorization = paste0("Bearer ", token))
-      )
-      ghql_gh_cli$load_schema()
-      ghql_gh_cli <<- ghql_gh_cli
+  token <- get_token()
 
-    }
-    return(ghql_gh_cli)
+  if(!exists("ghql_gh_cli")){
+    ghql_gh_cli <- ghql::GraphqlClient$new(
+      url = "https://api.github.com/graphql",
+      headers = httr::add_headers(Authorization = paste0("Bearer ", token))
+    )
+    ghql_gh_cli$load_schema()
+    ghql_gh_cli <<- ghql_gh_cli
+
   }
-
+  return(ghql_gh_cli)
 }
